88eabe4c65dcc831cbf67cf4b83b5b047dcccd8e
mayerprog <mairuhin@mail.ru>
2022-10-27 21:32:46 +0300

    restructure folders

---
+++ other algorithm tasks/frequency-change/1.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+let data = fs.readFileSync('01 (1).txt', 'utf8');
+let testCase = data.split('\n')
+
+let result = 0;
+
+const finalResult = (testCase) => {
+    for (let i = 0; i < testCase.length; i++) {
+            result += parseInt(testCase[i], 10)
+        }
+        return result
+    }
+
+console.log(finalResult(testCase))
+
